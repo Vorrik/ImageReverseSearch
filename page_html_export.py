29cb34c2afee7eb7c36d19e98c512c37282daa88
Vorrik <moderatorxd66@gmail.com>
2025-05-18 15:50:44 +0300

    Added ID field

--- page_html_export.py
+++ page_html_export.py
@@ -11,10 +11,12 @@ class PageHTMLExport:
             self.template = file.read()
 
     def save(self, pages: List[Tuple[str, PairParseResult]], output: str) -> None:
-        table_rows = [(f'<tr><td><img src="{image_path}" width="100" height="100" alt=""/></td>'
+        table_rows = [(f'<tr><td>{i}</td>'
+                       f'<td><img src="{image_path}" width="100" height="100" alt=""/></td>'
                        f'<td><img src="{page.image.url}" width="100" height="100" alt=""/></td>'
                        f'<td>{page.page.title}</td>'
-                       f'<td><a href="{page.page.url}">{page.page.url}</a></td></tr>') for image_path, page in pages]
+                       f'<td><a href="{page.page.url}">{page.page.url}</a></td></tr>')
+                      for i, (image_path, page) in enumerate(pages)]
 
         with open(output, 'w', encoding='utf-8') as f:
             generated_page = self.template.replace('[ROWS]', ''.join(table_rows))
